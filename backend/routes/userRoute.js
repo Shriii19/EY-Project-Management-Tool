72adf808f24cab3f5a9dbc3b49783db279bd29a6
shriii19 <mudabeshrinivas@gmail.com>
2025-12-31 16:22:27 +0530

    feat: add user stats and activity routes to userRouter

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { updateProfile, getCurrentUser } from '../controller/userController.js';
+import { updateProfile, getCurrentUser, getUserStats, getUserActivity } from '../controller/userController.js';
 import { validateUserProfile } from '../middleware/validation.js';
 
 const userRouter = express.Router();
@@ -8,5 +8,7 @@ const userRouter = express.Router();
 
 userRouter.get('/me', getCurrentUser);
 userRouter.put('/profile', validateUserProfile, updateProfile);
+userRouter.get('/stats', getUserStats);
+userRouter.get('/activity', getUserActivity);
 
 export default userRouter;
